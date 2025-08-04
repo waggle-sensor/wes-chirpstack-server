6a7047a88f1b65590e3cc3364e96ee79d9fb01b1
FranciscoLozCoding <francisco.lozano33@outlook.com>
2025-08-04 16:36:55 -0500

    Update WireGuard configuration to use dynamic server IP for AllowedIPs

--- init-wireguard.sh
+++ init-wireguard.sh
@@ -32,6 +32,7 @@ NODE_WG_IP=$(echo $JSON | jq -r '.[0].node_wg_ip')
 SERVER_PUB_KEY=$(echo $JSON | jq -r '.[0].server_pub_key')
 SERVER_PUB_IP=$(echo $JSON | jq -r '.[0].server_pub_ip')
 SERVER_PORT=$(echo $JSON | jq -r '.[0].server_wg_port')
+SERVER_WG_IP=$(echo $JSON | jq -r '.[0].server_wg_ip')
 
 # Write wg0.conf
 cat <<EOF > /wireguard/config/wg0.conf
@@ -42,7 +43,7 @@ Address = $NODE_WG_IP
 [Peer]
 PublicKey = $SERVER_PUB_KEY
 Endpoint = $SERVER_PUB_IP:$SERVER_PORT
-AllowedIPs = 0.0.0.0/0
+AllowedIPs = $SERVER_WG_IP/32
 PersistentKeepalive = 25
 EOF
 
