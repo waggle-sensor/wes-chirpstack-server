e701c11baca6a426d5962f2ed762db372d60548f
FranciscoLozCoding <francisco.lozano33@outlook.com>
2025-08-06 10:01:12 -0500

    use manual instead of wg-quick

--- init-wireguard.sh
+++ init-wireguard.sh
@@ -53,9 +53,10 @@ if [ -z "$NODE_PRIV_KEY" ] || [ -z "$NODE_WG_IP" ] || [ -z "$SERVER_PUB_KEY" ] |
     exit 1
 fi
 
-# Write wg0.conf
+# Write config to file
 mkdir -p /wireguard/config
-WG_CONFIG="/wireguard/config/wg0.conf"
+IFACE=wg0
+WG_CONFIG="/wireguard/config/$IFACE.conf"
 echo "[WIREGUARD] Writing WireGuard config to $WG_CONFIG ..."
 cat <<EOF > $WG_CONFIG
 [Interface]
@@ -72,9 +73,15 @@ chmod 600 $WG_CONFIG
 
 # Start WireGuard
 echo "[WIREGUARD] Starting WireGuard with $WG_CONFIG ..."
-wg-quick up $WG_CONFIG
+# wg-quick up $WG_CONFIG
+ip link add dev $IFACE type wireguard
+ip address add dev $IFACE $NODE_WG_IP/30
+wg set $IFACE private-key $NODE_PRIV_KEY
+wg set $IFACE listen-port $SERVER_PORT
+ip link set up dev $IFACE
+wg set $IFACE peer $SERVER_PUB_KEY endpoint $SERVER_PUB_IP:$SERVER_PORT allowed-ips $SERVER_WG_IP/32 persistent-keepalive 25
 if [ $? -ne 0 ]; then
-    echo "[WIREGUARD] Failed to bring up WireGuard interface wg0. Exiting."
+    echo "[WIREGUARD] Failed to bring up WireGuard interface $IFACE. Exiting."
     exit 1
 fi
 echo "[WIREGUARD] WireGuard started successfully!"
